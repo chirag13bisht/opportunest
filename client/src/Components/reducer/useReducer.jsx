1040f2c1cf5c6125f2f1fcbbfa135a925da4173a
chirag13bisht <99008443+chirag13bisht@users.noreply.github.com>
2024-10-01 09:04:39 +0530

    Update useReducer.jsx

--- client/src/Components/reducer/useReducer.jsx
+++ client/src/Components/reducer/useReducer.jsx
@@ -1,10 +1,19 @@
-export const initialState = null;
+// src/Components/reducer/useReducer.js
 
+export const initialState = {
+    isAuthenticated: false,
+    user: null,
+};
 
-export const reducer = (state, action) =>{
-    if(action.type === "USER"){
-        return action.payload;
+export const reducer = (state, action) => {
+    switch (action.type) {
+        case 'USER':
+            return {
+                ...state,
+                isAuthenticated: action.payload.isAuthenticated,
+                user: action.payload.user,
+            };
+        default:
+            return state;
     }
-    return state;
-
-}
+};
